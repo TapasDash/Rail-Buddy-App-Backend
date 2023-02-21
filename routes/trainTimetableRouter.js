7e30ab4c5b431c551b4eec02e8b36bd077c07b6f
TapasDash <tapasdash017@gmail.com>
2023-02-21 11:59:49 +0530

    trains api

--- routes/trainTimetableRouter.js
+++ routes/trainTimetableRouter.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getTrainData } from "../controllers/trainController.js";
+import {
+  getTrainData,
+  getTrainsBetweenStations,
+} from "../controllers/trainController.js";
 
 const trainTimetableRouter = express.Router();
 
 trainTimetableRouter.route("/train").get(getTrainData);
+trainTimetableRouter
+  .route("/trains/:fromStation/:toStation")
+  .get(getTrainsBetweenStations);
 
 export default trainTimetableRouter;
