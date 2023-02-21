851361be884d587233716f1ba779dac9316f7f25
TapasDash <tapasdash017@gmail.com>
2023-02-21 12:17:29 +0530

    trains api bw stns

--- controllers/trainController.js
+++ controllers/trainController.js
@@ -3,10 +3,60 @@ import TrainTimetable from "../models/TrainTimetable.js";
 
 export const getTrainData = asyncHandler(async (req, res) => {
   console.log(req.query);
-  const data = await TrainTimetable.find(req.query);
-  console.log({ data });
+  const data = await TrainTimetable.find(req.query).sort("seq");
   return res.status(200).json({
     success: true,
     data,
   });
 });
+
+export const getTrainsBetweenStations = asyncHandler(async (req, res) => {
+  const { fromStation, toStation } = req.params;
+  const data1 = await TrainTimetable.find({
+    stationCode: fromStation,
+    destinationStation: toStation,
+  });
+  const data2 = await TrainTimetable.find({
+    stationCode: fromStation,
+    sourceStation: toStation,
+  });
+  const data3 = await TrainTimetable.find({
+    sourceStation: fromStation,
+    stationCode: toStation,
+  });
+
+  const data = [...data1, ...data2, data3];
+  return res.status(200).json({
+    success: true,
+    data,
+  });
+});
+
+// export const getSourcesData = asyncHandler(async (req, res) => {
+//   const { fromStation, toStation } = req.params;
+//   const data = await Customer.aggregate()
+//     .match({ dealership })
+//     .facet({
+//       data1: [
+//         {
+//           $match: {
+//             stationCode: fromStation,
+//             destinationStation: toStation,
+//           },
+//         },
+//       ],
+//       data2: [
+//         {
+//           $match: {
+//             stationCode: fromStation,
+//             destinationStation: toStation,
+//           },
+//         },
+//       ],
+//     });
+
+//   return res.status(200).json({
+//     success: true,
+//     tableData: customerData,
+//   });
+// });
